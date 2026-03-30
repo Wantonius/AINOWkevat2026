6dcdf970d349046385b078606b28464f3afe5884
Erno Hentonen <erno.hentonen@opiframe.com>
2026-03-30 12:56:35 +0300

    first commit

---
+++ javascript/02_forms/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",express.static("frontend"));
+
+app.use(express.json());
+
+app.post("/greet",function(req,res) {
+	console.log(req);
+	const name = req.body.firstname+" "+req.body.lastname;
+	return res.status(201).json({"Greetings":name});
+})
+
+console.log("Running in port 3000");
+
+app.listen(3000);
